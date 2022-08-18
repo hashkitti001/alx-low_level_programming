5a065f78f264248c20e9ebfaf7160a3e0bc08856
IwegbuJeddy <iJeddy5.0@gmail.com>
2022-08-17 17:38:37 -0700

    refix

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,17 +1,16 @@
-#include "dog.h"
 #include <stdlib.h>
-/* free_dog - name of function
- * @dog_t - 
- * @d -
- * return - void
-*/
+#include "dog.h"
 
+/**
+ * free_dog - frees memory allocated for a struct dog
+ * @d: struct dog to free
+ */
 void free_dog(dog_t *d)
 {
-	if (d)
-	{
-		free(d->name);
-		free(d->owner);
-		free(d);
-	}
+if (d)
+{
+free(d->name);
+free(d->owner);
+free(d);
+}
 }
