c867d08ec405beba0f0d210fdc4d700c10105e78
hashkitti001 <ijeddy5.0@gmail.com>
2023-06-15 11:03:59 -0700

    Added challenge 7 :tada:

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,8 +4,10 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void){
-	char *prompt = "with proper grammar, but the outcome is a piece of art, \n";
+int main(void)
+{
+	char *prompt = "with proper grammar, but the outcome is a piece of art,\n";
+
 	printf("%s", prompt);
 	return (0);
 }
