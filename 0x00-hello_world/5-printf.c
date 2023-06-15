144b7499d84fa93e5664b624ce7b0074915d0d0c
hashkitti001 <ijeddy5.0@gmail.com>
2023-06-15 10:59:00 -0700

    Added challenge 7 :tada:

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void){
+	char *prompt = "with proper grammar, but the outcome is a piece of art,";
+	printf("%s", prompt);
+return (0);
+}
