c5a80a8c318b181c5fc9c7d6c95436b10f172664
hashkitti001 <ijeddy5.0@gmail.com>
2023-06-16 06:11:43 -0700

    Added challenge 9 :tada:

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,13 +10,13 @@ int main(void)
 {
 	 int i = 0;
 
-	char CH = 'A';
+	char CH = 'a';
 
 	for (i = 0; i <= 9; i++)
 	{
 	putchar(i + '0');
 	}
-	while (CH < 'G')
+	while (CH < 'g')
 	{
 	putchar(CH);
 	CH++;
