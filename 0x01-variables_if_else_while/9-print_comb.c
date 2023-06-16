8370f0908bc952c2b36c1671346a101069ce29d5
hashkitti001 <ijeddy5.0@gmail.com>
2023-06-16 06:26:09 -0700

    Added challenge 10 :tada:

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: print 0, 1, - 9 with comma and spaces
+ *
+ *
+ * Return: Always 0 (Success)
+*/
 int main(void)
 {
 	int i = 0;
-	while (i < 10){
+
+	while (i < 10)
+	{
 	putchar(i + '0');
-	if(i != 9)
+	if (i != 9)
 	{
 	putchar(',');
 	putchar(' ');
