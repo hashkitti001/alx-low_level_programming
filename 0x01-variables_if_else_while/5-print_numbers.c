8e03671b0f81e92f657ce9458fe35afd44e60a1e
hashkitti001 <ijeddy5.0@gmail.com>
2023-06-16 05:40:41 -0700

    Added challenge 6 :tada:

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: print all numbers under Base 10
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int i = 0;
+
+	for (i = 0; i < 10; i++)
+	{
+	printf("%d", i);
+	}
+        printf("\n");
+	return (0);
+}
