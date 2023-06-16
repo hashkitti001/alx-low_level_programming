867e6d980c7eb6a373ec3d1376169b804d9a9dc1
hashkitti001 <ijeddy5.0@gmail.com>
2023-06-16 05:54:46 -0700

    Added challenge 7 :tada:

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -8,13 +8,13 @@
 */
 int main(void)
 {
-        int i = 0;
+	int i = 0;
 
-        for (i = 0; i < 10; i++)
-        {
-        putchar(i + '0');
-        }
-        putchar('\n');
-        return (0);
+	for (i = 0; i < 10; i++)
+	{
+	putchar(i + '0');
+	}
+	putchar('\n');
+	return (0);
 }
 
