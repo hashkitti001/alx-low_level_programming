fbd49e5bb02bb900de4191828ace6fe7a82768e4
hashkitti001 <ijeddy5.0@gmail.com>
2023-06-16 07:30:49 -0700

    Added challenge 11 :tada:

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print all possible different
+ *           combinations of two digits.
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int i = 0, j;
+
+	for(i = 0; i <= 9; i++)
+	{
+		j = 0;
+		for(j = 0; j <= 9; j++)
+		{
+			if (i != j && i <= j)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+
+				if (i != '8' && i != '9')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			
+		}
+		
+	}
+	putchar('\n');
+
+	return (0);
+}
