cd39707d9995afcfb90be5566b4fbc398993baa7
hashkitti001 <ijeddy5.0@gmail.com>
2023-06-20 12:17:09 -0700

    Added challenge 16 :tada:

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add -Adds two numbers
+ *
+ * @a : First number
+ *
+ * @b : Second number
+ *
+ * @sum : The sum
+ */
+int add(int a, int b)
+	{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+	}
