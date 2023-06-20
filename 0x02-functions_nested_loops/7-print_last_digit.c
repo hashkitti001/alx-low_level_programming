af425571dd7da6da77f3737b88736b6830874a78
hashkitti001 <ijeddy5.0@gmail.com>
2023-06-20 11:52:44 -0700

    Added challenge 13 :tada:

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_last_digit - Prints last digit 
+ * print_last_digit - Prints last digit
  * @n: The integer to be computed.
  *
  * Return: The absolute value of the integer.
@@ -11,7 +11,7 @@ int print_last_digit(int n)
 
 	lastDigit = n % 10;
 
-	if(lastDigit < 0)
+	if (lastDigit < 0)
 		lastDigit *= -1;
 	_putchar(lastDigit + '0');
 	return (lastDigit);
