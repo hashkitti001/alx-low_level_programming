d8943331bfd9dad48a1549576a5e177b7511533c
hashkitti001 <ijeddy5.0@gmail.com>
2023-06-20 08:35:52 -0700

    Putchar challenge solution

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 /**
  *
  * Description : Prints putchar 
@@ -7,6 +7,14 @@
  **/ 
 int main (void)
 {
-	putchar('_putchar\n');
-return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
