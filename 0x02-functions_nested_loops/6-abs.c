5af1bb93ce1558716ed7f14e5b58f260f2e6ae08
hashkitti001 <ijeddy5.0@gmail.com>
2023-06-20 11:26:37 -0700

    Added challenge 12 :tada:

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,12 +3,12 @@
  * abs - Prints the sign of a number.
  * @n: The number of which the sign will be printed.
  *
- * Return: The absolute value of a number 
+ * Return: The absolute value of a number
  */
 
 int _abs(int n)
 {
-	if(n < 0)
+	if (n < 0)
 	{
 	return (n * -1);
 	}
