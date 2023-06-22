608d72984bd0fec4808a82c1a82daf7e98b008fe
hashkitti001 <ijeddy5.0@gmail.com>
2023-06-22 01:06:18 -0700

    Added challenge 12 :tada:

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main (void)
+{
+long int i, num;
+
+num = 612852475143;
+	for (i = 2; i <= n; i++)
+	{
+		if (n % i == 0)
+		{
+			n = n / i;
+			i--;
+		}
+	}
+	printf("%li\n", i);
+return (0);
+}
