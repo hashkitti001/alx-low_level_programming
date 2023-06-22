a828cb642c9f0357405af5a753573b5b388bf1ff
hashkitti001 <ijeddy5.0@gmail.com>
2023-06-22 01:10:02 -0700

    Added challenge 13 :tada:

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_number - short description, single line
+ * @n: description of parameter a
+(* a blank line
+ * description: longer description of the function)?
+(* section header: section description)*
+ * return: void
+*/
+void print_number(int n)
+{
+	_putchar(n + '0');
+}
