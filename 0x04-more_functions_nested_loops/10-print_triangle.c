4d21b8681e368858a25962511258a1e477d3a7d0
hashkitti001 <ijeddy5.0@gmail.com>
2023-06-22 06:44:11 -0700

    Added challenge 18 :tada:

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,22 +1,33 @@
 #include "main.h"
+
 /**
- * print_number - prints an integer
- * @n:.input integer parameter
- *
+ * print_triangle - entry point
+ * Description: Prints diagonals
+ *@size: size of the triangle
+ * Return: void
  */
-void print_number(int n)
+
+void print_triangle(int size)
 {
-	unsigned int i = n;
+	int row, hashes, spaces;
 
-	if (n < 0)
+	if (size <= 0)
 	{
-		_putchar(45);
-		i = -i;
+		_putchar('\n');
 	}
-	if (i / 10)
+	else
 	{
-		print_number(i / 10);
+		for (row = 1; row <= size; row++)
+		{
+			for (spaces = size - row; spaces >= 1; spaces--)
+			{
+				_putchar(' ');
+			}
+			for (hashes = 1; hashes <= row; hashes++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
 	}
-	_putchar(i % 10 + '0');
 }
-
