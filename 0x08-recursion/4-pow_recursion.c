8d743289010709fbdd9d4d5ed7f085a735aea56f
hashkitti001 <ijeddy5.0@gmail.com>
2023-07-05 10:21:25 -0700

    Added challenge 4 :tada:

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _pow_recursion - function recursive
+ * @x: Integer
+ * @y: Integer
+ * Return: result
+*/
+int _pow_recursion(int x, int y)
+{
+int powered;
+if (y < 0)
+{
+return (-1);
+}
+else 
+{
+powered = x * _pow_recursion(x, y - 1);
+}
+return (result);
+}
