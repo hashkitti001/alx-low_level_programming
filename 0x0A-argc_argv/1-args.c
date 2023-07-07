342658d1928b2570dd028636ab30619c01adf139
hashkitti001 <ijeddy5.0@gmail.com>
2023-07-07 05:02:00 -0700

    Added challenge 1 :tada:

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Prints the argument count of a program
+ * @argc: Argument count 
+ * @argv: Arguments
+ * Return 0
+*/
+int main(int argc, char ** argv)
+{
+(void)argv;
+printf("%d", argc);
+return (0);
+}
