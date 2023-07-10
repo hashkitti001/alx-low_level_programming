ca902bf56b0d86f8aa7f5667c9a4d295116cc958
hashkitti001 <ijeddy5.0@gmail.com>
2023-07-10 09:30:19 -0700

    Added challenge 3 :tada:

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "main.h"
+/* _strdup - Returns a pointer to a  new string which is a clone
+ * @str: a string 
+ * Return: NULL or ar
+*/
+char *_strdup(char *str)
+{
+	char *pointer;
+
+	if (str == NULL)
+		return (NULL);
+	pointer = malloc(sizeof(char));
+	return (pointer);
+}
