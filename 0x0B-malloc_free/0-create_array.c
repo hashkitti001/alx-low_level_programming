dea9aa283a4639c6dc4b976492cfd36d7cd25c89
hashkitti001 <ijeddy5.0@gmail.com>
2023-07-10 09:22:55 -0700

    Added challenge 2 :tada:

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,31 +1,28 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * create_array - writes the character c to stdout 
- * @size: unsigned int 
- * @c:Character 
- * Return : NULL or ar
-*/  
+ * create_array - dynamically allocates memory
+ * for a string c and rpints character c to stdout
+ * @size: unsigned int
+ * @c:Character
+ * Return: NULL or ar
+*/
 char *create_array(unsigned int size, char c)
 {
-	
 	char *ar;
 
 	unsigned int i;
 
 	if (size == 0)
-	{
-	return (NULL);
-	}
-	ar = malloc(size * sizeof(char));
-	if (ar == NULL)
-	{
-	return (NULL);
-	}
-	for (i = 0; i <= size; i++)
-	{
-	ar[i] = c;
-	}
-	return (ar);
+		return (NULL);
 
+	ar = malloc(size * sizeof(char));
+
+	if (ar == NULL)
+		return (NULL);
+
+	for (i = 0; i <= size; i++)
+		ar[i] = c;
+
+	return (ar);
 }
