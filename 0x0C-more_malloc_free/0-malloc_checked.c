ef7522d45497ebd5acf23e97154ba8fc237dbb19
hashkitti001 <ijeddy5.0@gmail.com>
2023-07-14 07:46:45 -0700

    Added challenge 1 :tada:

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+#define ERRNO 98
+/**
+ * malloc_checked - Allocates memory of magnitude/size b
+ * @b: Size of memory allocated
+ * Return: Pointer or 98
+ *
+*/
+void *malloc_checked(unsigned int b)
+{
+void *q;
+q = malloc(b);
+if (q == NULL)
+{
+exit(ERRNO);
+}
+else
+{
+return (p);
+}
+}
