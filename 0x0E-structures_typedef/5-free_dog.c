0f6eb68427f226942164342864b0ad4cf0badf2b
hashkitti001 <ijeddy5.0@gmail.com>
2023-07-16 23:56:17 -0700

    Added challenge 4 :tada:

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdlib.h>
+/* free_dog - name of function
+ * @dog_t - Structure
+ * @d - Structure instance
+ * return - void
+*/
+
+void free_dog(dog_t *d)
+{
+	if (d != 0)
+	{
+		free(d.name);
+		free(d.owner);
+		free(d);
+	}
+}
