66e5e10899d5d029a292ef3665335b687731f3d6
hashkitti001 <ijeddy5.0@gmail.com>
2023-07-17 07:00:58 -0700

    Added challenge 5 :tada:

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,23 @@
 #include <stdio.h>
 #ifndef DOG
 #define DOG
+/**
+ * struct dog - structure
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+*/ 
 typedef struct dog
 {
 char *name;
 float age;
 char *owner;
 } dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
+
+void free_dog(dog_t *d);
+
 #endif
