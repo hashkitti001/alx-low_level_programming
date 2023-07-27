f2f03da7584bda58ce435f54564d58c45b72f174
hashkitti001 <ijeddy5.0@gmail.com>
2023-07-27 06:45:40 -0700

    Added a function that prints the number of items in a linked list

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,14 +1,14 @@
 #include "lists.h"
 /**
  * list_len - returns the number of elements in a linked list
- * @h: Linked list 
+ * @h: Linked list
  * Return: number of elements in list
 */
 size_t list_len(const list_t *h)
 {
-int numElems = 0; 
+int numElems = 0;
 
-while(h != NULL)
+while (h != NULL)
 	{
 	numElems++;
 	h = h->next;
