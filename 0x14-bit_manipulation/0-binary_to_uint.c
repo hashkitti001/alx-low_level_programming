4c0143340630da58cc84d883f4393a773438d195
hashkitti001 <iJeddy5.0@gmail.com>
2023-11-23 09:41:38 -0800

    Second attempt on task 1

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,24 +1,29 @@
-#include <stdio.h>
+#include "main.h"
 
-unsigned int binary_to_uint(const char *b) {
-    if (b == NULL)
-        return 0;
+/**
+ * binary_to_uint -converts a binary number to an unsigned int.
+ * @b: pointing to a string of 0 and 1 chars
+ *
+ * Return:converted number, or 0
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int i;
+	unsigned int j;
 
-    unsigned int result = 0;
-
-    // Check each character in the string
-    while (*b != '\0') {
-        // If the character is not '0' or '1', return 0 (invalid)
-        if (*b != '0' && *b != '1')
-            return 0;
-
-        // Convert character to integer value
-        int digit = *b - '0';
-
-        // Shift the result to the left and add the current digit
-        result = (result << 1) + digit;
-
-        b++; // Move to the next character
-    }
-	return result;
+	j = 0;
+	if (!b)
+		return (0);
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+	}
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		j <<= 1;
+		if (b[i] == '1')
+			j += 1;
+	}
+	return (j);
 }
